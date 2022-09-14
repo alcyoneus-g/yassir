994855ef07dac8fab1aa99fd3d5eb147ad5097c6
jacob-liu-g <spring.scene@hotmail.com>
2022-09-13 20:31:14 -0400

    remove dev condition for worker

--- src/index.tsx
+++ src/index.tsx
@@ -11,10 +11,10 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 import theme from './theme';
 
-if (process.env.NODE_ENV === 'development') {
+// if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./mocks/browser')
   worker.start()
-}
+// }
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
